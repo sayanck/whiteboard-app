64fcd1540d2df8d6e36f18d042b126fb2aab4f28
Sayan Chakraborty <108548778+sayanC12@users.noreply.github.com>
2023-12-19 22:01:05 +0530

    updates

---
+++ app/src/main/java/com/cmt/taxcalculate/MainActivity.java
@@ -0,0 +1,41 @@
+package com.cmt.taxcalculate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button b1;
+    Button b2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        b1 = findViewById(R.id.button1);
+        b2 = findViewById(R.id.button2);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(MainActivity.this, com.cmt.taxcalculate.IncomeActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this, com.cmt.taxcalculate.EMIActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+}
